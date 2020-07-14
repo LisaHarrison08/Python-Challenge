066a114a2c27bffcdb737d96617d41c491f34b31
Lisa Harrison <lmharrison0810@gmail.com>
2020-07-14 10:52:44 -0400

    main.py file created

---
+++ PyBank/main.py
@@ -0,0 +1,12 @@
+import os
+import csv
+
+csvpath = "/Users/LmH80/Desktop/DataViz/Homework/Python-Challenge/PyBank/Resources/budget_data.csv"
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+
+    for row in csvreader:
+        print(row)
+
