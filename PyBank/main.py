c6be19ec29c028688f139c69dad5bbdc88c2b3eb
Lisa Harrison <lmharrison0810@gmail.com>
2020-07-16 12:08:22 -0400

    DictReader applied, changes in profit/loss and average amount of period calculated

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,50 +1,60 @@
 #The total number of months in dataset
 #The net total amount of profit/losses over the netire period
-# ---- totalPl_amount = sum int(row[2])
-    #print (totalPl_amount)
 #The average of the changes in profit/losses over the entire period
-#--- def average (row[2])
-        #averageP_L =sum(row[2])/len(row[2])
-        #return averageP-L
-
 #The greatest increase in profits(data&amount) over entire period
 #The greatest decrease in losses(data&amount) over entire period
 
 import os
 import csv
+#defaultdict knows to expect integers as values and starts at 0
+# from collections import defaultdict
 
 csvpath = os.path.join(".","PyBank","Resources","budget_data.csv")
 
-#Set the variables
+#Set the variables - later update to counts=defaultdict(int)?
 totalAmount = 0
 totalMonths = 0
+profitLoss_change = 0
+profitLoss_prev = 0
+profitLoss_change = []
 
-#Read the csv file
+#Read the csv file and initialize DictReader for csv key value pairs
 with open(csvpath) as budget:
-    csvreader = csv.reader(budget, delimiter=',')
+    csvreader = csv.DictReader(budget, delimiter=',')
     # print(csvreader)
 
 #run the following code to view the header values
     # csvheader = next(csvreader)
     # print(f'CSV_Header:{csvheader}')
 
-#run the following code to skip the header values
-    csv_header=next(csvreader, None)
+#Do not need to run the following code to skip the header values as using DictRead
+    # csv_header=next(csvreader, None)
 
+# Loop through data and calculate the totals
     for row in csvreader:
-        #print(row)
-         #months = row[0]
         totalMonths = totalMonths +1
-        profitLoss = int(row[1])
+        profitLoss = int(row["Profit/Losses"])
 
-# calculate the total number of months
-        # numberMonths += months
-        # print (totalMonths)
+#Calculate the total amount profit/losses += increments the row
+        totalAmount += int(row["Profit/Losses"])
 
-#calculate the total amount profit/losses
-        totalAmount += int(profitLoss)
+#Track the changes in profit/losses over the entire period - += increments each PL row
+        profitLoss_prev += int(row["Profit/Losses"])
+        profitLoss_change = int(row["Profit/Losses"]) - profitLoss_prev
+
+#Calculate the average of the changes in profit/losses over the entire period      
+        average = totalAmount/totalMonths
+# Round the average amount to two decimal places
+        average = round (average,2)
+#The greatest increase in profits(data&amount) over entire period
+
+#The greatest decrease in losses(data&amount) over entire period
+
+# Set print statements outside of loop to output totals        
 print(f'The total number of months:{totalMonths}')
 print(f'The total amount:{totalAmount}')       
+print(f'The changes in Profit/Losses:{profitLoss_change}')
+print(f'The average changes in Profit/Losses:{average}')
 
-
+# Set file to output analysis
 
